e791dd8ca0bd39496c7bb49b00fe5c892102cc52
MalinduJayasinghe <jayasinghemalindu3@gmail.com>
2026-05-28 09:58:42 +0530

    initial commit

---
+++ src/main/java/lk/ijse/AAD_75/Aad75Application.java
@@ -0,0 +1,13 @@
+package lk.ijse.AAD_75;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aad75Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aad75Application.class, args);
+	}
+
+}
