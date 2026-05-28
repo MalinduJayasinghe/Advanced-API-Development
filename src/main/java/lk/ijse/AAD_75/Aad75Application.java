e81db7c24ed449f6100a990fa076a3b0cb0f79ce
MalinduJayasinghe <jayasinghemalindu3@gmail.com>
2026-05-28 10:27:27 +0530

    altered code to add database

--- src/main/java/lk/ijse/AAD_75/Aad75Application.java
+++ src/main/java/lk/ijse/AAD_75/Aad75Application.java
@@ -3,10 +3,14 @@ package lk.ijse.AAD_75;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@Configuration - mark as a config class
+//@ComponentScan - find all the controllers and services automatically//
+//@EnableAutoConfiguration - Spring automatically configures dependencies
 @SpringBootApplication
 public class Aad75Application {
 
 	public static void main(String[] args) {
+		// .run command starts our application
 		SpringApplication.run(Aad75Application.class, args);
 	}
 
